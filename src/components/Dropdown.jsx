dec384e17a7f1e0fcc0fca0a8708c4a330800fc1
RHShoumik <rhshoumik12@gmail.com>
2023-08-01 14:40:45 +0600

    Taxt overlay problem solved

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -33,7 +33,7 @@ const Dropdown = (props) => {
     };
 
     return (
-        <div ref={dropdownRef} className="relative inline-block text-left">
+        <div ref={dropdownRef} className="inline-block text-left">
             <div
                 onClick={toggleDropdown}
                 className="inline-flex justify-center items-center gap-1 px-4 py-2 text-sm font-medium "
@@ -42,42 +42,46 @@ const Dropdown = (props) => {
                 {dropdownTextColor ? (
                     <p className="text-[16px] text-white">{dropdownText}</p>
                 ) : (
-                    <p className="text-[16px] text-white dark:text-secondary">{dropdownText}</p>
+                    <p className="text-[16px] text-white dark:text-secondary">
+                        {dropdownText}
+                    </p>
                 )}
                 {rightIcon ? rightIcon : ""}
             </div>
-            {isOpen && (
-                <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                    <div
-                        className="py-1"
-                        role="menu"
-                        aria-orientation="vertical"
-                        aria-labelledby="options-menu"
-                    >
-                        {items.map((item) => (
-                            <a
-                                href="#"
-                                className="block px-4 py-2 text-sm text-gray-700 border-b-[1px] hover:bg-gray-100 hover:text-gray-900 last:border-b-0"
-                                role="menuitem"
-                                key={item.id}
-                            >
-                                <div className="flex">
-                                    {dropdownIcon ? (
-                                        <img
-                                            src={item.icon}
-                                            className="w-7 mr-4"
-                                            alt="Icon"
-                                        />
-                                    ) : (
-                                        ""
-                                    )}
-                                    <p className="">{item.name}</p>
-                                </div>
-                            </a>
-                        ))}
+            <div className="relative">
+                {isOpen && (
+                    <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                        <div
+                            className="py-1"
+                            role="menu"
+                            aria-orientation="vertical"
+                            aria-labelledby="options-menu"
+                        >
+                            {items.map((item) => (
+                                <a
+                                    href="#"
+                                    className="block px-4 py-2 text-sm text-gray-700 border-b-[1px] hover:bg-gray-100 hover:text-gray-900 last:border-b-0"
+                                    role="menuitem"
+                                    key={item.id}
+                                >
+                                    <div className="flex">
+                                        {dropdownIcon ? (
+                                            <img
+                                                src={item.icon}
+                                                className="w-7 mr-4"
+                                                alt="Icon"
+                                            />
+                                        ) : (
+                                            ""
+                                        )}
+                                        <p className="">{item.name}</p>
+                                    </div>
+                                </a>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 };
